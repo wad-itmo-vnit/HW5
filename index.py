a5f6744f4f7d0bfbe6e4d0bcffd4cc80a506659d
nguyentienlong1106 <tienlong1106@gmail.com.vn>
2021-08-17 01:34:08 +0300

    homework05

--- index.py
+++ index.py
@@ -1,10 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, request, redirect, make_response
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def home():
+    return redirect('/login'), 301
+def auth(request):
+    token = request.coookies.get('login-info')
+    try:
+        user, pwd = token.split(':')
+    except:
+        return False
+    if (user == 'admin' and pwd == 'admin'):
+        return True
+    else: 
+        return False
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        user = request.form.get("uname")
+        pwd = request.form.get("pwd")
+        if (user == 'admin' and pwd == 'admin'):
+            token = user + ":" + pwd
+            resp = make_response(redirect('/index'))
+            resp.set_cookie('login_info', token)
+            return render_template('index.html')
+        else:
+            return redirect('/login'), 403
+
+
+
+@app.route('/index')
 def open_web():
-    return render_template('index.html')
+    if auth(request):
+        return render_template('index.html')
+    else:
+        return redirect('/')
 
-app.run(host='localhost', port=5000)
+
+
+@app.route('/img/get/<number>')
+def serve_img(number):
+    file_name = './static/' + number + '.jpg'
+    return send_file(file_name)
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000)
